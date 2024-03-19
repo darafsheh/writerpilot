c9389ab9d88a564fff7e458966c1450fa349b283
Shar Dara <shar@stripe.com>
2024-03-18 22:57:02 -0400

    gating system 2

--- components/ui/Pricing/Pricing.tsx
+++ components/ui/Pricing/Pricing.tsx
@@ -26,7 +26,7 @@ interface SubscriptionWithProduct extends Subscription {
   prices: PriceWithProduct | null;
 }
 interface EntitlementsWithFeatures extends Entitlement {
-  entitlements: Entitlement[];
+  access: Entitlement[];
 }
 
 interface Props {
