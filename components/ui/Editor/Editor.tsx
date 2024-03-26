a8f595ebc7475a525ba516a46af58293d7c9635a
Shar Dara <shar@shars-air.mynetworksettings.com>
2024-03-25 22:28:10 -0400

    v1 final fixes

--- components/ui/Editor/Editor.tsx
+++ components/ui/Editor/Editor.tsx
@@ -173,65 +173,61 @@ export default function Editor({ user, products, subscription, entitlements }: P
                                 </div>
 
                             )
-                        }
-                        { //feature?.lookup_key == "basic" && 
-                            (
-                                <div className=''>
-                                    <form action="#" className="relative bg-gray-800">
-                                        <div className="overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
-                                        
-                                            <textarea
-                                            rows={10}
-                                            name="title"
-                                            id="title"
-                                            className="h-52 pl-5 block bg-gray-800 w-full border-0 pt-2.5 text-lg font-medium placeholder:text-gray-600 focus:ring-0 text-gray-200"
-                                            placeholder="You have access to the AI editor. Write a your content here..."
-                                            />
-
-                                            {/* Spacer element to match the height of the toolbar */}
-                                            <div aria-hidden="true">
-                                            <div className="py-2">
-                                                <div className="h-9" />
-                                            </div>
-                                            <div className="h-px" />
-                                            <div className="py-2">
-                                                <div className="py-px">
-                                                <div className="h-9" />
-                                                </div>
-                                            </div>
-                                            </div>
-                                        </div>
-
-                                        <div className="absolute inset-x-px bottom-0">
-                                           
-                                            <div className="flex items-center justify-between space-x-3 border-t border-gray-200 px-2 py-2 sm:px-3">
-                                            <div className="flex">
-                                                <button
-                                                type="button"
-                                                className="group -my-2 -ml-2 inline-flex items-center rounded-full px-3 py-2 text-left text-gray-400"
-                                                >
-                                                <PaperClipIcon className="-ml-1 mr-2 h-5 w-5 group-hover:text-gray-500" aria-hidden="true" />
-                                                <span className="text-sm italic text-gray-500 group-hover:text-gray-600">Attach a file</span>
-                                                </button>
-                                            </div>
-                                            <div className="flex-shrink-0">
-                                                <button
-                                                type="submit"
-                                                className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                                >
-                                                Save
-                                                </button>
-                                            </div>
-                                            </div>
-                                        </div>
-                                        </form>
-                                </div>
-                            )
-                        }
+                        } 
                       </div>
                     )
                   })
                 }
+                <div className=''>
+                    <form action="#" className="relative bg-gray-800">
+                        <div className="overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
+                        
+                            <textarea
+                            rows={10}
+                            name="title"
+                            id="title"
+                            className="h-52 pl-5 block bg-gray-800 w-full border-0 pt-2.5 text-lg font-medium placeholder:text-gray-600 focus:ring-0 text-gray-200"
+                            placeholder="You have access to the AI editor. Write a your content here..."
+                            />
+
+                            {/* Spacer element to match the height of the toolbar */}
+                            <div aria-hidden="true">
+                            <div className="py-2">
+                                <div className="h-9" />
+                            </div>
+                            <div className="h-px" />
+                            <div className="py-2">
+                                <div className="py-px">
+                                <div className="h-9" />
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div className="absolute inset-x-px bottom-0">
+                            
+                            <div className="flex items-center justify-between space-x-3 border-t border-gray-200 px-2 py-2 sm:px-3">
+                            <div className="flex">
+                                <button
+                                type="button"
+                                className="group -my-2 -ml-2 inline-flex items-center rounded-full px-3 py-2 text-left text-gray-400"
+                                >
+                                <PaperClipIcon className="-ml-1 mr-2 h-5 w-5 group-hover:text-gray-500" aria-hidden="true" />
+                                <span className="text-sm italic text-gray-500 group-hover:text-gray-600">Attach a file</span>
+                                </button>
+                            </div>
+                            <div className="flex-shrink-0">
+                                <button
+                                type="submit"
+                                className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                Save
+                                </button>
+                            </div>
+                            </div>
+                        </div>
+                        </form>
+                </div>
               </div>
             </div>
 
